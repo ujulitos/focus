6d3e35db472345fbb9fe08896811c8426dcb479e
ujulitos <ejs7599@yahoo.com>
2022-10-16 23:16:28 -0500

    16

---
+++ Didi/T2_MetodoPER/js/paginas.js
@@ -0,0 +1,17 @@
+const paginas = [];
+
+function generarPaginas() {
+    const totalPaginas = 5;
+
+    for (let contador = 1; contador <= totalPaginas; contador++) {
+        let nombrePagina;
+
+        if (contador < 10) {
+            nombrePagina = `pagina0${contador}.html`;
+        } else {
+            nombrePagina = `pagina${contador}.html`;
+        }
+
+        paginas.push(nombrePagina);
+    }
+}
