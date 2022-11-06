890b29f77a1d351c579b9e35bd615443b33f4007
ujulitos <ejs7599@yahoo.com>
2022-11-06 04:40:34 -0600

    06

---
+++ Didi/ArbolDecisiones/Farming/js/app.js
@@ -0,0 +1,87 @@
+import {
+    App
+} from "./clases/App.js";
+import {
+    modoDesarrollador,
+    tiempo,
+    redimensionar,
+    moverTeclas,
+    botonesTemario
+} from "./funciones.js";
+import {
+    btnAdelante,
+    btnAtras,
+    btnMenu,
+    btnCerrar,
+    btnAudio,
+    btnRecargar,
+    btnCerrarGlosario,
+    contGlosario,
+    btnGlosario
+} from './selectores.js';
+import {
+    registrarTiempo,
+    cerrarConexion
+} from './conexion.js';
+
+//Instanciar App e iniciarla------------------------------------------------------
+export const aplicacion = new App;
+aplicacion.iniciar();
+
+// Funciones de los botones de navegación-----------------------------------------
+btnAdelante.click(function() {
+    aplicacion.nextPag();
+});
+
+btnAtras.click(function() {
+    aplicacion.prevPag();
+});
+
+btnMenu.click(function() {
+    aplicacion.desplegarMenu();
+});
+
+btnCerrar.click(function() {
+    aplicacion.cerrarMenu();
+});
+
+btnAudio.click(function() {
+    aplicacion.silenciarOPrender();
+});
+
+btnRecargar.click(function() {
+    aplicacion.recargarPag();
+});
+
+//Función Glosario Prosa----------------------------------------------------------
+btnGlosario.click(function() {
+    contGlosario.css('display', 'flex');
+});
+
+btnCerrarGlosario.click(function() {
+    contGlosario.css('display', 'none');
+});
+
+botonesTemario();
+
+//Al cerrar el curso
+window.onunload = function() {
+    registrarTiempo(tiempo);
+    cerrarConexion();
+}
+
+// Modo desarrollador-------------------------------------------------------------
+// modoDesarrollador();
+moverTeclas();
+
+//Redimensionar tamaño------------------------------------------------------------
+redimensionar();
+let intervalo;
+$(window).resize(function() {
+    clearTimeout(intervalo);
+    intervalo = setTimeout(escalar, 300);
+});
+
+function escalar() {
+    aplicacion.recargarPag();
+}
