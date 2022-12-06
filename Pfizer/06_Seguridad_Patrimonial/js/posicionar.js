0ebfb5a735ae6197189f738ed38b55c9134be8e2
ujulitos <ejs7599@yahoo.com>
2022-12-06 02:44:32 -0600

    06

---
+++ Pfizer/06_Seguridad_Patrimonial/js/posicionar.js
@@ -0,0 +1,31 @@
+let elementos;
+
+// Al cargarse la página
+$(window).on("load", function() {
+    posicionarElementos();
+});
+
+function posicionarElementos() {
+    elementos = $('.elementos');
+
+    $.each(elementos, function() {
+        const posicionX = $(this).data('posx');
+        const posicionY = $(this).data('posy');
+
+        $(this).css({
+            'top': `${posicionY}px`,
+            'left': `${posicionX}px`
+        });
+    });
+}
+
+// Al redimensionar la página
+let intervaloR2;
+$(window).resize(function() {
+    clearTimeout(intervaloR2);
+    intervaloR2 = setTimeout(escalarElementos, 300);
+});
+
+function escalarElementos() {
+    // redimensionarElementos();
+}
