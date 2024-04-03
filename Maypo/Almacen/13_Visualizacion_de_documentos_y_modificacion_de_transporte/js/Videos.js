ae5c43dd74608cd5f28ae04fdf3497617377fb5c
ujulitos <ejs7599@yahoo.com>
2024-04-03 17:16:10 -0600

    03

---
+++ Maypo/Almacen/13_Visualizacion_de_documentos_y_modificacion_de_transporte/js/Videos.js
@@ -0,0 +1,47 @@
+﻿var currentVideo;
+var currentCallBack;
+var pausadoVideo = false;
+
+function pausaVideo() {
+    console.log('pausaVideo');
+    // clearTimeout(elRetardo);
+    // buzz.all().pause();
+    pausadoVideo = true;
+}
+
+function despausaVideo() {
+    console.log('despausaVideo');
+    if (currentVideo != null || currentVideo != undefined || currentVideo != '') {
+        playVideo(currentVideo, 0, currentCallBack);
+    }
+}
+
+function setVolumen() {
+    buzz.all().setVolume(volPage);
+}
+
+$(document).ready(function() {
+    setVolumen();
+
+    // grupoVideos.load();
+    // grupoVideos.bind("loadstart", function() {
+    // grupoVideosMin.load();
+    // grupoVideosMin.bind("loadstart", function() {
+    // console.log('loadstart');
+    // });
+    var cuantosVideos = grupoVideos.length;
+    // var cuantosVideos = 1;
+    console.log('cuantosVideos', cuantosVideos);
+    var contador = 0;
+    // grupoVideosMin.bind("loadeddata", function() {
+    // grupoVideos.bind("loadeddata", function() {
+    //     contador++;
+    //     // console.log('loadeddata', contador);
+    //     if (contador >= cuantosVideos) {
+    //         console.log('todos los Videos cargados.');
+    //         // Videos_cargados();
+    //     }
+    // }).bind("error", function() {
+    //     console.log('hubo un error');
+    // });
+});
